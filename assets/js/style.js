f6a564a0ea10733123732a8b04d7250badaf9864
guptan <nguptaa183@gmail.com>
2017-10-17 18:24:56 +0530

    added

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(window).on("scroll",function(){
   	var wn = $(window).scrollTop();
     if(wn > 208){
-    	$(".navbar").css("background","rgba(0,0,0,1)");
+    	$(".navbar").css("background","rgba(0,0,0,0.9)");
     }
     else{
     	$(".navbar").css("background","rgba(0,0,0,0)");
