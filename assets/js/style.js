226d237378ac9b0163f872f73a1a6238918d510e
guptan <nguptaa183@gmail.com>
2017-10-20 14:01:15 +0530

    added

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
-	$(window).on("scroll",function(){
-  	var wn = $(window).scrollTop();
-    if(wn > 208){
-    	$(".navbar").css("background","rgba(0,0,0,0.9)");
+  $(window).on("scroll",function(){
+    var wn = $(window).scrollTop();
+    if(wn > 215){
+      $(".navbar").css("background","rgba(0,0,0,0.9)");
     }
     else{
-    	$(".navbar").css("background","rgba(0,0,0,0)");
+      $(".navbar").css("background","rgba(0,0,0,0)");
     }
   });
 });
+
+
+// var totalCount = 10;
+// function ChangeIt()
+// {
+// var num = Math.ceil( Math.random() * totalCount );
+// $('first').css('background-image', 'url(../img/DynamicCover/' + num + '.jpg)');
+// }
+
+//
+// var loading = function () {
+//   var totalCount = 10;
+//   var num = Math.floor(Math.random() * totalCount) + 1;
+//   $('first').css('background-image', '#ffff');
+//
+// }
