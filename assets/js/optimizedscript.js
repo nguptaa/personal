d38d43b93ff108a88e07d3f172d8e3f9a5c877bd
guptan <nguptaa183@gmail.com>
2017-10-20 20:13:33 +0530

    added

---
+++ assets/js/optimizedscript.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(window).on("scroll",function(){var wn=$(window).scrollTop();if(wn>215){$(".navbar").css("background","rgba(0,0,0,0.9)");}else{$(".navbar").css("background","rgba(0,0,0,0)");}});});
