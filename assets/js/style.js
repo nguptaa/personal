fe0653d1c03ed4b556c06b0164ceb096d0092b2a
guptan <nguptaa183@gmail.com>
2017-10-21 09:30:12 +0530

    added

--- assets/js/style.js
+++ assets/js/style.js
@@ -9,19 +9,3 @@ $(document).ready(function(){
     }
   });
 });
-
-
-// var totalCount = 10;
-// function ChangeIt()
-// {
-// var num = Math.ceil( Math.random() * totalCount );
-// $('first').css('background-image', 'url(../img/DynamicCover/' + num + '.jpg)');
-// }
-
-//
-// var loading = function () {
-//   var totalCount = 10;
-//   var num = Math.floor(Math.random() * totalCount) + 1;
-//   $('first').css('background-image', '#ffff');
-//
-// }
