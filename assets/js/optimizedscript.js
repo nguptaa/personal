df2b87f53190566c08906da367cf3d6f7f92d3f2
guptan <nguptaa183@gmail.com>
2017-10-22 17:24:46 +0530

    Changes

--- assets/js/optimizedscript.js
+++ assets/js/optimizedscript.js
@@ -1,4 +1,1 @@
 $(document).ready(function(){$(window).on("scroll",function(){var wn=$(window).scrollTop();if(wn>215){$(".navbar").css("background","rgba(0,0,0,0.9)");}else{$(".navbar").css("background","rgba(0,0,0,0)");}});});
-window.onload = function() { document.body.className = ''; }
-			window.ontouchmove = function() { return false; }
-			window.onorientationchange = function() { document.body.scrollTop = 0; }
