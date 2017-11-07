95b3428e1b589c6f6ad983a717b0802e0bd03a28
guptan <nguptaa183@gmail.com>
2017-11-07 16:23:10 +0530

    changes

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108453170-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108453170-1');
+  </script>
+  <!-- Title -->
+  <title>Nikhil Gupta - Tech Enthusiast</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- favicon -->
+  <link rel="icon" type="image/gif" href="assets/img/favicon.ico" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- styling -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <!-- fontawesome -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- particles style -->
+  <link rel="stylesheet" type="text/css" href="assets/css/particles.css">
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108453170-1"></script>
-  <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-108453170-1');
-  </script>
-  <!-- Title -->
-  <title>Nikhil Gupta - Tech Enthusiast</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- favicon -->
-  <link rel="icon" type="image/gif" href="assets/img/favicon.ico" />
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <!-- styling -->
-  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-  <!-- fontawesome -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!-- particles style -->
-  <link rel="stylesheet" type="text/css" href="assets/css/particles.css">
-  <!-- font -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
-</head>
-<body>
+<?php include "includes/header.php" ?>
 
   <!-- navbar -->
   <nav class="navbar fixed-top navbar-expand-md navbar-dark">
@@ -234,3 +204,5 @@
 
 </body>
 </html>
+
+<?php include "includes/footer.php" ?>
