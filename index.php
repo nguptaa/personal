05548b1b26a923189d43d7f715df462a47bcc273
guptan <nguptaa183@gmail.com>
2017-11-07 16:24:00 +0530

    changes

--- index.php
+++ index.php
@@ -1,187 +1,187 @@
 <?php include "includes/header.php" ?>
 
-  <!-- navbar -->
-  <nav class="navbar fixed-top navbar-expand-md navbar-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="assets/img/home.svg" height="25" width="25" /></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item link">
-            <a class="nav-link" href="#about">About</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="#skill">Skills</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="#">Projects</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="#">Blog</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- header img -->
-  <section class="first"></section>
-
-  <!-- particles body  -->
-  <div id="particles-js"></div>
-
-  <!-- container open -->
+<!-- navbar -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark">
   <div class="container">
-      <!-- Nikhil avatar -->
-      <div class="avatar text-center">
-        <img src="assets/img/avatar.jpg" class="img-fluid rounded-circle" width="160" height="160"/>
-        <h3 id="about">Nikhil Gupta</h3>
-        <h6>I believe in Technology and Innovation</h6>
-        <p class="key">
-          Tech Enthusiast | Learner | Coder | Web Developer
-        </p>
-
-        <!-- social icons -->
-            <ul class="nav justify-content-center">
-              <li class="nav-item social">
-                <a class="nav-link" href="https://www.facebook.com/nguptaa183" target="_blank"><i class="fa fa-facebook" style="font-size:27px;color:#3b5999;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="https://twitter.com/ngupta183" target="_blank"><i class="fa fa-twitter" style="font-size:27px;color:#55acee;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="https://www.linkedin.com/in/nikhil-gupta-aab490140" target="_blank"><i class="fa fa-linkedin" style="font-size:27px;color:#0077B5;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="https://www.slack.com" target="_blank"><i class="fa fa-slack" style="font-size:27px;color:#3aaf85;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="https://www.github.com/nguptaa" target="_blank"><i class="fa fa-github" style="font-size:27px;color:#333;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="https://www.quora.com/profile/Nikhil-Gupta-397" target="_blank"><i class="fa fa-quora" style="font-size:27px;color:#b92b27;"></i></a>
-              </li>
-              <li class="nav-item social">
-                <a class="nav-link" href="mailto:nguptaa183@gmail.com" target="_blank"><i class="fa fa-envelope" style="font-size:27px;color:#FF8800;"></i></a>
-              </li>
-            </ul>
-          </div>
-
-
-
-
-    <!-- content -->
-    <div class="content">
-      <div class="row">
-        <!-- Programming/ -->
-        <div class="col-sm-6 col-md-6">
-          <div class="d-block mb-4">
-            <span class="skillbar">Programming/Dev</span>
-            <div class="progress">
-              <div class="progress-bar green" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">PYTHON</span>
-                <span class="pull-right">75%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar blue" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">C</span>
-                <span class="pull-right">65%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar yellow" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">C++</span>
-                <span class="pull-right">60%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar red" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">PHP/SQL</span>
-                <span class="pull-right">70%</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Designing -->
-        <div class="col-sm-6 col-md-6">
-          <div class="d-block mb-4">
-            <span class="skillbar">Designing</span>
-            <div class="progress">
-              <div class="progress-bar green" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">HTML5</span>
-                <span class="pull-right">90%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar blue" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">CSS3</span>
-                <span class="pull-right">85%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar yellow" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">BOOTSTRAP</span>
-                <span class="pull-right">80%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar red" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">JAVASCRIPT</span>
-                <span class="pull-right">70%</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Databases-->
-        <div class="col-sm-6 col-md-6">
-          <div class="d-block mb-4">
-            <span class="skillbar">Database</span>
-            <div class="progress">
-              <div class="progress-bar bg-info" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">MYSQL</span>
-                <span class="pull-right">85%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar purple" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">MONGODB</span>
-                <span class="pull-right">75%</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Application Software-->
-        <div class="col-sm-6 col-md-6">
-          <div class="d-block mb-4">
-            <span class="skillbar">Software</span>
-            <div class="progress">
-              <div class="progress-bar bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">MS (WORD/POWERPOINT/EXCEL)</span>
-                <span class="pull-right">80%</span>
-              </div>
-            </div>
-            <div class="progress">
-              <div class="progress-bar cyan" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                <span class="pull-left">PHOTOSHOP</span>
-                <span class="pull-right">70%</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <p class="text-center">Also familiar with <strong>Sony Vegas Pro</strong>, <strong>Final Cut Pro X</strong>
-      </p>
+    <a class="navbar-brand" href="#"><img src="assets/img/home.svg" height="25" width="25" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item link">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="#skill">Skills</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
     </div>
-    <!-- container closed -->
+  </div>
+</nav>
+
+<!-- header img -->
+<section class="first"></section>
+
+<!-- particles body  -->
+<div id="particles-js"></div>
+
+<!-- container open -->
+<div class="container">
+  <!-- Nikhil avatar -->
+  <div class="avatar text-center">
+    <img src="assets/img/avatar.jpg" class="img-fluid rounded-circle" width="160" height="160"/>
+    <h3 id="about">Nikhil Gupta</h3>
+    <h6>I believe in Technology and Innovation</h6>
+    <p class="key">
+      Tech Enthusiast | Learner | Coder | Web Developer
+    </p>
+
+    <!-- social icons -->
+    <ul class="nav justify-content-center">
+      <li class="nav-item social">
+        <a class="nav-link" href="https://www.facebook.com/nguptaa183" target="_blank"><i class="fa fa-facebook" style="font-size:27px;color:#3b5999;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="https://twitter.com/ngupta183" target="_blank"><i class="fa fa-twitter" style="font-size:27px;color:#55acee;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="https://www.linkedin.com/in/nikhil-gupta-aab490140" target="_blank"><i class="fa fa-linkedin" style="font-size:27px;color:#0077B5;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="https://www.slack.com" target="_blank"><i class="fa fa-slack" style="font-size:27px;color:#3aaf85;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="https://www.github.com/nguptaa" target="_blank"><i class="fa fa-github" style="font-size:27px;color:#333;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="https://www.quora.com/profile/Nikhil-Gupta-397" target="_blank"><i class="fa fa-quora" style="font-size:27px;color:#b92b27;"></i></a>
+      </li>
+      <li class="nav-item social">
+        <a class="nav-link" href="mailto:nguptaa183@gmail.com" target="_blank"><i class="fa fa-envelope" style="font-size:27px;color:#FF8800;"></i></a>
+      </li>
+    </ul>
   </div>
 
-  <!-- Horizontal line -->
-  <hr>
+
+
+
+  <!-- content -->
+  <div class="content">
+    <div class="row">
+      <!-- Programming/ -->
+      <div class="col-sm-6 col-md-6">
+        <div class="d-block mb-4">
+          <span class="skillbar">Programming/Dev</span>
+          <div class="progress">
+            <div class="progress-bar green" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">PYTHON</span>
+              <span class="pull-right">75%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar blue" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">C</span>
+              <span class="pull-right">65%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar yellow" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">C++</span>
+              <span class="pull-right">60%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar red" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">PHP/SQL</span>
+              <span class="pull-right">70%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Designing -->
+      <div class="col-sm-6 col-md-6">
+        <div class="d-block mb-4">
+          <span class="skillbar">Designing</span>
+          <div class="progress">
+            <div class="progress-bar green" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">HTML5</span>
+              <span class="pull-right">90%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar blue" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">CSS3</span>
+              <span class="pull-right">85%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar yellow" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">BOOTSTRAP</span>
+              <span class="pull-right">80%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar red" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">JAVASCRIPT</span>
+              <span class="pull-right">70%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Databases-->
+      <div class="col-sm-6 col-md-6">
+        <div class="d-block mb-4">
+          <span class="skillbar">Database</span>
+          <div class="progress">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">MYSQL</span>
+              <span class="pull-right">85%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar purple" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">MONGODB</span>
+              <span class="pull-right">75%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Application Software-->
+      <div class="col-sm-6 col-md-6">
+        <div class="d-block mb-4">
+          <span class="skillbar">Software</span>
+          <div class="progress">
+            <div class="progress-bar bg-secondary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">MS (WORD/POWERPOINT/EXCEL)</span>
+              <span class="pull-right">80%</span>
+            </div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar cyan" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <span class="pull-left">PHOTOSHOP</span>
+              <span class="pull-right">70%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="text-center">Also familiar with <strong>Sony Vegas Pro</strong>, <strong>Final Cut Pro X</strong>
+    </p>
+  </div>
+  <!-- container closed -->
+</div>
+
+<!-- Horizontal line -->
+<hr>
 
 <?php include "includes/footer.php" ?>
