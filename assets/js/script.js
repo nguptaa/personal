6ec781d198056a0f14390b4f2506eb8ebcfa296e
guptan <nguptaa183@gmail.com>
2017-11-19 10:56:26 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,5 @@ $(document).ready(function(){
 
 // tooltip
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-});
-
-// loadbar
-$(document).ready(function(){
-
-var div_box="<div id='load-screen'><div id='loading'></div></div>";
-$("body").prepend(div_box);
-
-$('#load-screen').delay(700).fadeOut(600,function(){
-  $(this).remove();
-});
-
+    $("[data-toggle='tooltip']").tooltip();
 });
