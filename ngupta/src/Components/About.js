3b7155862acf4bf749662d7b6da448600f185f96
guptan <nguptaa183@gmail.com>
2019-07-05 11:29:05 +0545

    changes

--- ngupta/src/Components/About.js
+++ ngupta/src/Components/About.js
@@ -1,37 +1,29 @@
 import React from "react";
+import './About.css';
 
 class About extends React.Component {
   render() {
     return (
-      <div className="">
-        <p>
-          Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam
-          alia facete scriptorem, est autem aliquip detraxit at. Usu ocurreret
-          referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque
-          electram, eos choro alterum definiebas in. Vim dolorum definiebas an.
-          Mei ex natum rebum iisque.
-        </p>
-        <p>
-          Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam
-          alia facete scriptorem, est autem aliquip detraxit at. Usu ocurreret
-          referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque
-          electram, eos choro alterum definiebas in. Vim dolorum definiebas an.
-          Mei ex natum rebum iisque.
-        </p>
-        <p>
-          Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam
-          alia facete scriptorem, est autem aliquip detraxit at. Usu ocurreret
-          referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque
-          electram, eos choro alterum definiebas in. Vim dolorum definiebas an.
-          Mei ex natum rebum iisque.
-        </p>
-        <p>
-          Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam
-          alia facete scriptorem, est autem aliquip detraxit at. Usu ocurreret
-          referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque
-          electram, eos choro alterum definiebas in. Vim dolorum definiebas an.
-          Mei ex natum rebum iisque.
-        </p>
+      <div>
+        <section class="aboutme" id="about">
+          <div class="container text-center">
+            <i class="fas fa-user fa-4x" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor <br />
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam,
+              <br /> quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat.
+              <br />
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+              <br />
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </section>
       </div>
     );
   }
