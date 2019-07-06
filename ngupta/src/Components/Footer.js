dca8ca783d67fc20496b71fd8fd9a769af4521e8
guptan <nguptaa183@gmail.com>
2019-07-06 22:12:10 +0545

    changes

---
+++ ngupta/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <footer class="footer text-center">
+        <div class="container">
+          <hr />
+          <p class="text-muted small mb-0">
+            Copyright &copy; Nikhil Gupta 2019
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Projects;
