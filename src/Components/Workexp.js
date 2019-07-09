fe283dc19bf10bd235653af7d73f332e0cc9c780
guptan <nguptaa183@gmail.com>
2019-07-09 12:42:00 +0545

    changes

--- src/Components/Workexp.js
+++ src/Components/Workexp.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    marginBottom: "3rem !important"
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary
+    flexGrow: 1
   },
   card: {
-    maxWidth: 345
+    maxWidth: "100%"
+  },
+  media: {
+    height: 200
   }
 }));
 
@@ -30,65 +24,67 @@ export default function Workexp() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="md">
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Propelld"
-                  height="140"
-                  image={require("../Static/propelld.jpg")}
-                  title="Propelld"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Propelld"
-                  height="140"
-                  image={require("../Static/propelld.jpg")}
-                  title="Propelld"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        </Grid>
+    <section class="work-experience mb-5">
+      <div class="parallax2 mb-5">
+        <span>Work Experience</span>
       </div>
-    </Container>
+      <Container maxWidth="md">
+        <div className={classes.root}>
+          <Grid container spacing={10}>
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={require("../Static/kwoc.jpg")}
+                    title="kwoc"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Winter Open Source Development Intern, KWoC-IIT Kharagpur
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Contributed in developing student information portal of
+                      National Institute of Technology, Durgapur. Also added
+                      continuous integration and codecoverage in the github
+                      repository for better integration.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={require("../Static/propelld.jpg")}
+                    title="Propelld"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Web Developer Summer Intern, Propelld
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </section>
   );
 }
