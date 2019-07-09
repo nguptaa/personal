1dec4dc30a1246b9c9d8e8abd2c35c54363011ee
guptan <nguptaa183@gmail.com>
2019-07-09 15:45:04 +0545

    changes

--- src/Components/Workexp.js
+++ src/Components/Workexp.js
@@ -16,7 +16,9 @@ const useStyles = makeStyles(theme => ({
     textAlign: "center"
   },
   card: {
-    maxWidth: "100%"
+    maxWidth: "100%",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+
   },
   media: {
     height: 200
