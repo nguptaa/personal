174302acf1b710b9a32c170490cda543a33e7f6a
guptan <nguptaa183@gmail.com>
2019-07-11 19:48:48 +0545

    changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,7 +8,7 @@ import "./Projects.css";
 
 export default function Projects() {
   return (
-    <section class="projects">
+    <section className="projects">
       <div class="parallax1 mb-5">
         <span>Projects</span>
       </div>
@@ -19,12 +19,14 @@ export default function Projects() {
           date="2011 - present"
           iconStyle={{ background: "#4285F4", color: "#fff" }}
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
+            <h3 className="vertical-timeline-element-title">
+              Creative Director
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
