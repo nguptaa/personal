096957f42cc4165c88fc2f19af5bf71cbbc73ea1
guptan <nguptaa183@gmail.com>
2019-08-13 22:00:34 +0530

    changes

---
+++ build/precache-manifest.d3aabd59211004cdbf4259bdcc3db5eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c2d0cb98d28aee86dd94a99e41a7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0018cb26f335de6da365",
+    "url": "/static/css/2.d819a5bb.chunk.css"
+  },
+  {
+    "revision": "8e99f1632072f6a91c33",
+    "url": "/static/css/main.bee40fba.chunk.css"
+  },
+  {
+    "revision": "0018cb26f335de6da365",
+    "url": "/static/js/2.4f4cc636.chunk.js"
+  },
+  {
+    "revision": "8e99f1632072f6a91c33",
+    "url": "/static/js/main.7c64d63c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37ba966b4f235bcc473d9bb6a601759f",
+    "url": "/static/media/aboutme.37ba966b.png"
+  },
+  {
+    "revision": "6871ae919b1eea4fd45d97b1ee6d1e8d",
+    "url": "/static/media/bgmain.6871ae91.jpeg"
+  },
+  {
+    "revision": "6ca3aa74af682e0ddeed658cd2ab6896",
+    "url": "/static/media/kwoc.6ca3aa74.jpg"
+  },
+  {
+    "revision": "d51199429c181cef1696cef6bf610b1e",
+    "url": "/static/media/me.d5119942.jpeg"
+  },
+  {
+    "revision": "f220b360bdef869e90b3aacca7395789",
+    "url": "/static/media/projects2.f220b360.png"
+  },
+  {
+    "revision": "30495c8d502c1c27e319b19d15b56ae7",
+    "url": "/static/media/propelld.30495c8d.jpg"
+  },
+  {
+    "revision": "0ca800e18748bed42de6f1954e66b0a9",
+    "url": "/static/media/resume2.0ca800e1.png"
+  },
+  {
+    "revision": "908f1a71429531a78c37d001b726aafd",
+    "url": "/static/media/work2.908f1a71.png"
+  }
+]);
