b8cdcabcbae20577c95ef14b0f2b6b6f1716c47e
guptan <nguptaa183@gmail.com>
2020-07-01 21:27:58 +0545

    Added Social Links

--- src/components/landing/Contact/styles.js
+++ src/components/landing/Contact/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  padding: 4rem 0;
+  padding: 4rem 0 0;
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
