4e7f8b348d358c5107d1d69be80cf3a5f361f767
guptan <nguptaa183@gmail.com>
2020-07-03 13:41:18 +0545

    Modified 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
+import './404.css';
 
 export default () => (
   <Layout>
     <SEO title="404: Not found" location="/404" />
-    <div className="bg-main-404">
-      <div className="bg-content-404"></div>
+    <div className="content-404">
+      <h1>NOT FOUND</h1>
+      <p>
+        You just hit a route that doesn&#39;t exist... Please go back to{' '}
+        <a href="https://ngupta.me" rel="noopener noreferrer">
+          Home
+        </a>
+        .
+      </p>
     </div>
   </Layout>
 );
